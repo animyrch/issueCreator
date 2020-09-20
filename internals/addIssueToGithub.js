3809b59bd4a171cd5bcae764ed325621c3ab0b16
animyrch <info@traductionenturc.fr>
2020-09-20 21:37:37 +0200

    Removed console logs.

--- internals/addIssueToGithub.js
+++ internals/addIssueToGithub.js
@@ -2,7 +2,6 @@ const axios = require('axios');
 const githubURL = "https://api.github.com/graphql";
 
 const addIssueToGithub = async (mutation, token) => {
-  console.log(mutation, token);
   return axios.post(githubURL, {query: mutation}, {headers: token})
     .then(({ data }) => data.data)
     .catch(error => {
--- internals/getRepoFromGithub.js
+++ internals/getRepoFromGithub.js
@@ -2,7 +2,6 @@ const axios = require('axios');
 const githubURL = "https://api.github.com/graphql";
 
 const getRepoFromGithub = async (query, token) => {
-  console.log(query, token);
   return axios.post(githubURL, {query: query}, {headers: token})
     .then(({ data }) => data.data)
     .catch(error => {
