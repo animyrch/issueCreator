5cfe527bda4f94124d10bf0785a486c3f6bfcb7f
animyrch <info@traductionenturc.fr>
2020-09-20 14:19:21 +0200

    discovering cli functionalities

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-//. index 
-console.log('This is a message from the demo package');
+#!/usr/bin/env node
+
+const prompt = require('prompt');
+const promptAttributes = [
+  {
+    name: 'repository'
+  },
+  {
+    name: 'token'
+  }
+];
+
+prompt.start();
+
+prompt.get(promptAttributes, (error, { repository, token }) => {
+  if (error) {
+    console.log(error);
+    return 1;
+  } else {
+    console.log(repository);
+    console.log(token);
+  }
+});
+
